ca8c302bd1343ce326846690e489ea636cd4dd84
Ivan Spada <ivanspada.mail@gmail.com>
2025-03-27 19:30:45 +0100

    web app structure: 3 errors fixed

--- src/app/solmix/page.tsx
+++ src/app/solmix/page.tsx
@@ -4,6 +4,7 @@ import NavbarTool from "@/app/ui/navbar-tool";
 import { Tabs, TabList, Tab, TabPanel } from 'react-tabs';
 import { useState } from "react";
 import Solidity from "@/app/solmix/solidity/page";
+import Link from "next/link";
 
 export default function SolmixHome() {
     const [state, setState] = useState("");
@@ -17,7 +18,7 @@ export default function SolmixHome() {
                 </TabList>
                 <TabPanel>
                     <div className="w-full min-h-screen grid">
-                        <div className="bg-pink-500 solmix-tools p-5">
+                        <div className="solmix-tools p-5">
                             <BlocklyEditor/>
                         </div>
                     </div>
