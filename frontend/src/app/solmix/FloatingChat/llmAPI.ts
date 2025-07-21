47dd7f3d2519134c32939e62687a24f173e45540
Ivan Spada <ivanspada.mail@gmail.com>
2025-07-21 19:08:39 +0200

    fastapi -> smart constract: edit, comment, explain

--- frontend/src/app/solmix/FloatingChat/llmAPI.ts
+++ frontend/src/app/solmix/FloatingChat/llmAPI.ts
@@ -1,83 +1,60 @@
 'use server'
 
-import OpenAI from "openai";
+function extractSolidityCode(text: string) {
+    const regex = /```solidity\s*([\s\S]*?)\s*```/;
+    text = text.replace(/\\n/g, "\n")
+        .replace(/\\t/g, "\t")
+        .replace(/\\"/g, '"');
+    const match = text.match(regex);
 
-// import {PoeClient} from "poe-node-api";
-// https://www.npmjs.com/package/poe-node-api
-
-// import { PoeApi } from "poe-api-js";
-// https://github.com/yuangwei/poe-api-js
-
-function RemoveSolidityDeclaration(x: string): string {
-    const lines = x.split('\n');
-
-    if (lines.length > 1) {
-        lines.shift(); // Remove first line
-        lines.pop();   // Remove last line
-    }
-    return lines.join('\n')
+    return match ? match[1] : "Error: no generated code";
 }
 
-export async function sendLLMessage(message: string, current_solidity_code: string) {
-    // https://platform.openai.com/docs/overview
-    const openai_key = process.env.OPENAI_KEY
-    const client = new OpenAI({ apiKey: openai_key });
-
-    const prompt = `User request:\n${message}
-    \n\nYou are a smart contract and solidity expert, process the user request editing the following smart contract in solidity:
-    \n${current_solidity_code}
-    \n\nReturn only the Solidity code.`;  // todo: regex per estrarre il codice dalla pipeline
-
-    const response = await client.responses.create({
-        model: "gpt-4o-mini",
-        input: prompt,
+export async function editSmartContract(user_prompt: string, current_solidity_code: string) {
+    const newBody = {
+        message: user_prompt,
+        code: current_solidity_code
+    }
+    const res = await fetch('http://127.0.0.1:8000/edit', {
+        method: 'POST',
+        body: JSON.stringify(newBody),
+        headers: {
+            'Content-type': 'application/json'
+        }
     });
-    let answer = response.output_text;
+    let new_code = await res.text();
 
-    // remove first and last row (contains solidity declaration)
-    answer = RemoveSolidityDeclaration(answer)
-    return answer;
+    return extractSolidityCode(new_code);
 }
 
 export async function commentSmartContract(current_solidity_code: string) {
-    // https://platform.openai.com/docs/overview
-    const openai_key = process.env.OPENAI_KEY
-    const client = new OpenAI({ apiKey: openai_key });
-
-    const prompt = `You are a smart contract and solidity expert, add comments to the following smart contract in solidity in order to make it readable to not-skilled users:
-    \n${current_solidity_code}
-    \n\nReturn only the Solidity code.`;  // todo: regex per estrarre il codice dalla pipeline
-
-    const response = await client.responses.create({
-        model: "gpt-4o-mini",
-        input: prompt,
+    const newBody = {
+        code: current_solidity_code
+    }
+    const res = await fetch('http://127.0.0.1:8000/comment', {
+        method: 'POST',
+        body: JSON.stringify(newBody),
+        headers: {
+            'Content-type': 'application/json'
+        }
     });
-    let answer = response.output_text;
+    let new_code = await res.text()
 
-    // remove first and last row (contains solidity declaration)
-    answer = RemoveSolidityDeclaration(answer);  // TODO: use regex to extract solidity code
-    return answer;
+    return extractSolidityCode(new_code);
 }
 
 export async function explainSmartContract(current_solidity_code: string) {
-    // https://platform.openai.com/docs/overview
-    const openai_key = process.env.OPENAI_KEY
-    const client = new OpenAI({ apiKey: openai_key });
-
-    const prompt = `As a solidity expert, explain this smart contract in simple terms to someone new to blockchain development. Cover:
-    \n1. What this contract is designed to do
-    \n2. Its main purpose and goals
-    \n3. A beginner-friendly explanation of how it works internally
-    \n4. Any potential risks or important considerations
-    
-    Avoid technical jargon when possible, and when you must use technical terms, provide brief explanations. Use analogies where helpful. No intro is needed.
-    \n\nSmart Contract:
-    \n"""\n${current_solidity_code}\n"""`;
-
-    const response = await client.responses.create({
-        model: "gpt-4o-mini",
-        input: prompt,
+    const newBody = {
+        code: current_solidity_code
+    }
+    const res = await fetch('http://127.0.0.1:8000/explain', {
+        method: 'POST',
+        body: JSON.stringify(newBody),
+        headers: {
+            'Content-type': 'application/json'
+        }
     });
-    let answer = response.output_text;
-    return answer;
+    return (await res.text()).replace(/\\n/g, "\n")
+        .replace(/\\t/g, "\t")
+        .replace(/\\"/g, '"');;
 }
