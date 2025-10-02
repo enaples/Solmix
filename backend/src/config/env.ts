35bfab27e68ecf443bcf7d4573dab87c1ac0f598
enaples <enaples@users.noreply.github.com>
2025-10-02 17:08:08 +0200

    first step to refactor the backend

--- backend/src/config/env.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-const PORT = process.env.PORT || 3030;
-const SLITHER_CONTAINER_NAME = process.env.SLITHER_CONTAINER_NAME || "slither";
-const SLITHER_SC_PATH = process.env.SLITHER_SC_PATH || "/share/Solmix/contracts";
-
-export { PORT, SLITHER_CONTAINER_NAME, SLITHER_SC_PATH };
--- backend/src/routes/parseRoute.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { parseSolidity } from '../controllers/parseSolidity';
-
-const router = express.Router();
-
-router.post('/', parseSolidity); 
-
-export default router;
